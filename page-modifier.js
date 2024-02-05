9617425fa3d28567637703ed205cb6d766b91533
PurplePlane897 <158483594+PurplePlane897@users.noreply.github.com>
2024-02-05 20:23:55 +0100

    Update page-modifier.js

--- page-modifier.js
+++ page-modifier.js
@@ -1,87 +1,98 @@
-container_id = 'totk-btn-container'
-btn_id = 'totkguessr'
+(function() {
+    "use strict";
 
-forbidden_urls = [
-    'https://hyruleguessr.com/createChallenge',
-    'https://hyruleguessr.com/game',
-    'https://hyruleguessr.com/suggestion'
-]
+    const container_id = 'totk-btn-container'
+    const btn_id = 'totkguessr'
 
-const addButton = () => {
-    // If there is already a addButton, check if it need to be disabled
-    if (document.getElementById(container_id)) {
+    const forbidden_urls = [
+        'https://hyruleguessr.com/createChallenge',
+        'https://hyruleguessr.com/game',
+        'https://hyruleguessr.com/suggestion'
+    ]
+
+    // console.log("script init")
+
+    const addButton = () => {
+        // console.log('addButton()')
+        // If there is already a addButton, check if it need to be disabled
+        if (document.getElementById(container_id)) {
+            disableButton()
+            return
+        }
+        // console.log('will add the button')
+
+        let container = document.createElement('div')
+        container.id = container_id
+        container.classList.add("ml-auto", "mr-3", "navbar-text", "auth-text", "custom-control", "custom-switch")
+
+        let lb = document.createElement('label')
+        lb.innerText = 'TOTK'
+        lb.classList.add("custom-control-label")
+        lb.setAttribute("for", btn_id);
+        lb.style.cursor = "pointer"
+
+
+        let btn = document.createElement('input')
+        btn.type='checkbox'
+        btn.classList.add('custom-control-input')
+        btn.id = btn_id
+        btn.style.cursor = "pointer"
+
+        let checked = false
+        if (localStorage.getItem('totk') != "false"){
+            checked = true
+        }
+        btn.checked = checked
+
+        btn.addEventListener("change", (e) => {
+            if (canButtonBeUsed()){
+                // console.log("Changing storage var to " + e.target.checked)
+                localStorage.setItem('totk', e.target.checked)
+            }
+        });
+
+        container.appendChild(btn)
+        container.appendChild(lb)
+
+        let dark = document.getElementsByClassName('ml-auto')[0]
+        dark.insertAdjacentElement("beforebegin", container) 
+        dark.classList.remove("ml-auto")
+
         disableButton()
-        return
     }
-    nav = document.getElementsByClassName('navbar-custom')[0]
 
-    container = document.createElement('div')
-    container.id = container_id
-    container.classList.add("ml-auto", "mr-3", "navbar-text", "auth-text", "custom-control", "custom-switch")
-
-    lb = document.createElement('label')
-    lb.innerText = 'TOTK'
-    lb.classList.add("custom-control-label")
-    lb.setAttribute("for", btn_id);
-    lb.style.cursor = "pointer"
-
-
-    btn = document.createElement('input')
-    btn.type='checkbox'
-    btn.classList.add('custom-control-input')
-    btn.id = btn_id
-    btn.style.cursor = "pointer"
-
-    checked = false
-    if (localStorage.getItem('totk') != "false"){
-        checked = true
-    }
-    btn.checked = checked
-
-    btn.addEventListener("change", (e) => {
-        if (canButtonBeUsed()){
-            localStorage.setItem('totk', e.target.checked)
-            changeBG()
+    const disableButton = () => {
+        // console.log('disableButton()')
+        if (!document.getElementById('totk-btn-container')) {
+            return
         }
+        document.getElementById(btn_id).disabled = !canButtonBeUsed()
+    }
+
+    const canButtonBeUsed = () => {
+        for (let i = 0; i < forbidden_urls.length; i++) {
+            if (window.location.href.startsWith(forbidden_urls[i])){
+                // console.log('canButtonBeUsed() => false')
+                return false
+            }
+        }
+        // console.log('canButtonBeUsed() => true')
+        return true
+    }
+
+    // refresh button stat on document.location.href change
+    window.addEventListener("load", () => {
+        let oldHref = document.location.href;
+        const body = document.querySelector("body");
+        const observer = new MutationObserver(ms => {
+            if (oldHref !== document.location.href) {
+                oldHref = document.location.href;
+                // console.log('Location changed event')
+                addButton()
+            }
+        });
+        observer.observe(body, { childList: true, subtree: true });
     });
 
-    container.appendChild(btn)
-    container.appendChild(lb)
-
-    dark = document.getElementsByClassName('ml-auto')[0]
-    dark.insertAdjacentElement("beforebegin", container) 
-    dark.classList.remove("ml-auto")
-
-    disableButton()
-}
-
-const disableButton = () => {
-    if (!document.getElementById('totk-btn-container')) {
-        return
-    }
-    document.getElementById(btn_id).disabled = !canButtonBeUsed()
-}
-
-const canButtonBeUsed = () => {
-    for (let i = 0; i < forbidden_urls.length; i++) {
-        if (window.location.href.startsWith(forbidden_urls[i])){
-            return false
-        }
-    }
-    return true
-}
-
-// refresh button stat on document.location.href change
-window.addEventListener("load", () => {
-    let oldHref = document.location.href;
-    const body = document.querySelector("body");
-    const observer = new MutationObserver(ms => {
-        if (oldHref !== document.location.href) {
-            oldHref = document.location.href;
-            addButton()
-        }
-    });
-    observer.observe(body, { childList: true, subtree: true });
-});
-
-addButton()
+    addButton()
+})();
